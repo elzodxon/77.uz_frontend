3888d53b5f9560b7a0de4f675f32862670b8c8af
Iskandar-tulkinov <iscandardev@gmail.com>
2023-11-19 22:19:34 +0500

    feat: add comments on js

--- components/inputs/inputs.js
+++ components/inputs/inputs.js
@@ -1,6 +1,6 @@
 "use strict";
-// Password
 
+// Password
 document.addEventListener('DOMContentLoaded', function () {
   const eyeIcon = document.querySelector('.closed-eye');
   const passwordInput = document.getElementById('password');
@@ -15,7 +15,9 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   });
 });
+
 // Phone Number
+
 // Use let for variables that may be reassigned
 let phoneNumberInput = document.getElementById("phone-number");
 
@@ -30,9 +32,13 @@ phoneNumberInput.addEventListener("keydown", function (event) {
 });
 
 function formatPhoneNumber(input) {
+    // Remove non-numeric characters
     let phoneNumber = input.value.replace(/\D/g, '');
 
+
+    // Format the remaining digits
     if (phoneNumber.length > 0) {
+        // Format the phone number as (yy)xxx-xx-xx
         phoneNumber = '(' + phoneNumber.substring(0, 2) + ')' +
             phoneNumber.substring(2, 5) +
             '-' +
@@ -40,16 +46,18 @@ function formatPhoneNumber(input) {
             '-' +
             phoneNumber.substring(7, 9);
     }
-
+// Set the formatted value back to the input
     input.value = phoneNumber;
 }
 
 function handleBackspace(event, input) {
+    // Check if the pressed key is the backspace key
     if (event.key === 'Backspace') {
+        // Remove non-numeric characters
         let phoneNumber = input.value.replace(/\D/g, '');
-
+        // Remove the last digit
         phoneNumber = phoneNumber.slice(0, -1);
-
+// Format the remaining digits
         if (phoneNumber.length > 0) {
             phoneNumber = '(' + phoneNumber.substring(0, 2) + ')' +
                 phoneNumber.substring(2, 5) +
@@ -58,9 +66,9 @@ function handleBackspace(event, input) {
                 '-' +
                 phoneNumber.substring(7, 9);
         }
-
+// Set the formatted value back to the input
         input.value = phoneNumber;
-
+        // Prevent the default backspace behavior
         event.preventDefault();
     }
 }
