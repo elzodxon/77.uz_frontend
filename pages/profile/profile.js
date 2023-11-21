fbe947463e24a82cc537cbbfc962535014b3e9a5
Zohid-Jurakulovich <zohidjurakulovich@gmail.com>
2023-11-21 17:53:51 +0500

    Modify profile component and add profile page

---
+++ pages/profile/profile.js
@@ -0,0 +1,7 @@
+const languageButton = document.querySelector('.language-button');
+const languageContent = document.querySelector('.language-content');
+
+languageButton.addEventListener("click", (e) => {
+  e.preventDefault()
+  languageContent.classList.toggle("active")
+})
