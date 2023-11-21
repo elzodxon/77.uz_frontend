ccead847a87b190a0d8e0e436aec6d2e7cdd29b9
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2023-11-21 13:07:01 +0500

    added: ads component to home pagwe

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   // Get all elements with the class "category-group"
-  var categoryGroups = document.querySelectorAll(".category-group");
+  var categoryGroups = document.querySelectorAll('.category-group');
 
   // Add click event listener to each category group
   categoryGroups.forEach(function (categoryGroup) {
-    categoryGroup.addEventListener("click", function () {
+    categoryGroup.addEventListener('click', function () {
       // Toggle the "active" class on the clicked category group
-      this.classList.toggle("active");
+      this.classList.toggle('active');
     });
 
     // Get all elements with the class "icon-inner" inside each category group
-    var iconInners = categoryGroup.querySelectorAll(".icon-inner");
+    var iconInners = categoryGroup.querySelectorAll('.icon-inner');
 
     // Add click event listener to each icon-inner
     iconInners.forEach(function (iconInner) {
-      iconInner.addEventListener("click", function (event) {
+      iconInner.addEventListener('click', function (event) {
         event.stopPropagation(); // Prevents the click event from reaching the category-group
 
         // Toggle the "active" class on the closest ".category-union__item__inner"
-        this.closest(".category-union__item__inner").classList.toggle("active");
+        this.closest('.category-union__item__inner').classList.toggle('active');
       });
     });
   });
 });
 
-const searchInput = document.querySelector(".search__input")
-const blocker = document.querySelector(".blocker")
+const searchInput = document.querySelector('.search__input');
+const blocker = document.querySelector('.blocker');
+const favoriteProductButton = document.querySelectorAll(
+  '.favorite__product-button'
+);
+const favoriteProductPath = document.querySelectorAll('.favorite-path');
 
-searchInput.addEventListener("focus",(e) =>{
+for (let i = 0; i < favoriteProductButton.length; i++) {
+  favoriteProductButton[i].addEventListener('click', () => {
+    favoriteProductPath[i].classList.toggle('active');
+  });
+}
+
+searchInput.addEventListener('focus', (e) => {
   e.preventDefault();
-  blocker.classList.toggle("active")
-  document.body.classList.toggle("block")
-})
-blocker.addEventListener("click",() => {
-  document.body.classList.remove("block")
-  blocker.classList.remove("active")
-})
+  blocker.classList.toggle('active');
+  document.body.classList.toggle('block');
+});
+blocker.addEventListener('click', () => {
+  document.body.classList.remove('block');
+  blocker.classList.remove('active');
+});
