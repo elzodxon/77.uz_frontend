e6853c56229c75f3cc5e581e8513055f987592f7
zohida <zohishka04@gmail.com>
2023-11-21 10:37:46 +0500

    Modal info added

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,12 +1,15 @@
 
 let popup = document.getElementById("popup");
+let div = document.getElementById("div");
 
 function openPopup(){
     popup.classList.add("open-popup");
+    div.classList.add("active");
 }
 
 function closePopup(){
     popup.classList.remove("open-popup");
+    div.classList.remove("active");
 }
 
 
@@ -15,12 +18,40 @@ let poppup = document.getElementById("poppup");
 
 function opennPopup(){
     poppup.classList.add("open-popup");
+    div.classList.add("active");
 }
 
 function closeePopup(){
     poppup.classList.remove("open-popup");
+    div.classList.remove("active");
 }
 
+
+
+let application = document.getElementById("application");
+
+function openingPopup(){
+    application.classList.add("open-popup");
+    div.classList.add("active");
+}
+
+function closingPopup(){
+    application.classList.remove("open-popup");
+    div.classList.remove("active");
+}
+
+
+let accept = document.getElementById("accept");
+
+function openpopup(){
+    accept.classList.add("open-popup");
+    div.classList.add("active");
+}
+
+function closepopup(){
+    accept.classList.remove("open-popup");
+    div.classList.remove("active");
+}
 
 
 
