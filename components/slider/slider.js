cca24435a5ceb36b81f884ef828824d570d9ea55
Asranov Baxtinur <asranov0003@gmail.com>
2023-11-22 23:31:54 +0500

    render imgs using js

--- components/slider/slider.js
+++ components/slider/slider.js
@@ -1,35 +1,74 @@
 let currentIndex = 0;
 
-function showSlide(index) {
-  const container = document.querySelector(".slider__container");
-  const cards = document.querySelectorAll(".slider__card");
-  const items = document.querySelectorAll(".slider__item");
-  const itemWidth = items[0].offsetWidth;
+const images = [
+  "../../assets/img/SliderImg1.png",
+  "../../assets/img/SliderImg1.png",
+  "../../assets/img/SliderImg1.png",
+  "../../assets/img/SliderImg1.png",
+  "../../assets/img/SliderImg1.png",
+];
 
+function initializeSlider() {
+  renderSliderImages();
+  renderSliderCards();
+  showSlide(currentIndex);
+}
+
+function renderSliderImages() {
+  const container = document.getElementById("sliderContainer");
+
+  images.forEach((imageUrl, index) => {
+    const item = document.createElement("div");
+    item.classList.add("slider__item");
+
+    const img = document.createElement("img");
+    img.classList.add("slider__item-img");
+    img.src = imageUrl;
+    img.alt = `img ${index + 1}`;
+
+    item.appendChild(img);
+    container.appendChild(item);
+  });
+}
+
+function renderSliderCards() {
+  const container = document.getElementById("cardContainer");
+
+  images.forEach((imageUrl, index) => {
+    const card = document.createElement("div");
+    card.classList.add("slider__card");
+    card.onclick = () => showSlide(index);
+
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("slider__card-content");
+
+    const img = document.createElement("img");
+    img.classList.add("slider__card-img");
+    img.src = imageUrl;
+    img.alt = `img ${index + 1}`;
+
+    cardContent.appendChild(img);
+    card.appendChild(cardContent);
+    container.appendChild(card);
+  });
+}
+
+function showSlide(index) {
+  const container = document.getElementById("sliderContainer");
+  const cards = document.querySelectorAll(".slider__card");
+
+  const itemWidth = container.offsetWidth;
   container.style.transform = `translateX(-${index * itemWidth}px)`;
   currentIndex = index;
 
-  cards.forEach((card) => {
-    card.classList.remove("active");
+  cards.forEach((card, cardIndex) => {
+    card.classList.toggle("active", cardIndex === index);
   });
-
-  cards[index].classList.add("active");
-
-  items.forEach((item) => {
-    item.classList.remove("active");
-  });
-
-  items[index].classList.add("active");
 }
 
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + 4) % 4;
+function changeSlide(direction) {
+  currentIndex = (currentIndex + direction + images.length) % images.length;
   showSlide(currentIndex);
 }
 
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % 4;
-  showSlide(currentIndex);
-}
-
-showSlide(currentIndex);
+initializeSlider();
