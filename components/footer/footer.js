540605850056eff7b226794cdddddeabaf30e346
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2023-11-22 21:02:19 +0500

    changed footer data location

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,7 +1,21 @@
-import { footerList, generateListItem } from './footerList';
+const dataList = [
+  { text: 'Доска объявлений', link: '#' },
+  { text: 'Условия пользования', link: '#' },
+  { text: '+998 88 500 50 00', link: 'tel:+998885005000' },
+];
 
+function generateListItem(item) {
+  return `
+        <li class="footer-info__item">
+          <a href="${item.link}" class="footer-info__link">
+            <img src="../../assets/img/footer/symbol.svg" alt="" />
+            ${item.text}
+          </a>
+        </li>
+      `;
+}
 const ulElement = document.getElementById('footerList');
 
-footerList.forEach((item) => {
+dataList.forEach((item) => {
   ulElement.innerHTML += generateListItem(item);
 });
