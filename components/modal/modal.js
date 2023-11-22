291301e636a2bc53d7e796db029137c7d0a7ec29
zohida <zohishka04@gmail.com>
2023-11-22 23:29:34 +0500

    Modal finished

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,59 +1,17 @@
+const center = document.querySelector(".center"),
+    overlay = document.querySelector(".overlay"),
+    showBtn = document.querySelector(".btn"),
+    closeBtn = document.querySelector(".close-btn");
 
-let popup = document.getElementById("popup");
-let div = document.getElementById("div");
+    showBtn.addEventListener("click", () => center.classList.add("active"));
 
-function openPopup(){
-    popup.classList.add("open-popup");
-    div.classList.add("active");
-}
+    overlay.addEventListener("click", () =>
+        center.classList.remove("active")
+    );
 
-function closePopup(){
-    popup.classList.remove("open-popup");
-    div.classList.remove("active");
-}
-
-
-
-let poppup = document.getElementById("poppup");
-
-function opennPopup(){
-    poppup.classList.add("open-popup");
-    div.classList.add("active");
-}
-
-function closeePopup(){
-    poppup.classList.remove("open-popup");
-    div.classList.remove("active");
-}
-
-
-
-let application = document.getElementById("application");
-
-function openingPopup(){
-    application.classList.add("open-popup");
-    div.classList.add("active");
-}
-
-function closingPopup(){
-    application.classList.remove("open-popup");
-    div.classList.remove("active");
-}
-
-
-let accept = document.getElementById("accept");
-
-function openpopup(){
-    accept.classList.add("open-popup");
-    div.classList.add("active");
-}
-
-function closepopup(){
-    accept.classList.remove("open-popup");
-    div.classList.remove("active");
-}
-
-
+    closeBtn.addEventListener("click", () =>
+    center.classList.remove("active")
+);
 
 
 
