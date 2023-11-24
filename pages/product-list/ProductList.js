1e355a0bb85d840cbc37ed8093b935f4dd446fd2
Mirafzal0713 <usmonjonovmirafzal0713@gmail.com>
2023-11-24 18:01:02 +0500

    fix: grid and list style added

--- pages/product-list/ProductList.js
+++ pages/product-list/ProductList.js
@@ -1,21 +1,33 @@
 const languageButton = document.querySelector('.language-button')
 const languageContent = document.querySelector('.language-content')
-const header = document.getElementById('list__switcher')
-const icons = header.getElementsByClassName('switcher-icon')
+const switchers = document.querySelectorAll('.product-list__view-icons .product-list__view-icon')
+const view_wraps = document.querySelectorAll('.view-wrap')
+const list_view = document.querySelector('.list-view')
+const grid_view = document.querySelector('.grid-view')
 
 languageButton.addEventListener('click', e => {
   e.preventDefault()
   languageContent.classList.toggle('active')
 })
 
-for (var i = 0; i < icons.length; i++) {
-  icons[i].addEventListener('click', function () {
-    var current = document.querySelector('.active')
+switchers.forEach(function (switcher) {
+  switcher.addEventListener('click', function () {
+    switchers.forEach(function (switcher) {
+      switcher.classList.remove('active')
+    })
 
-    if (current) {
-      current.classList.remove('active')
+    switcher.classList.add('active')
+
+    var view = switcher.getAttribute('data-view')
+
+    view_wraps.forEach(function (view) {
+      view.style.display = 'none'
+    })
+
+    if (view == 'list-view') {
+      list_view.style.display = 'block'
+    } else {
+      grid_view.style.display = 'block'
     }
-
-    this.classList.add('active')
   })
-}
+})
