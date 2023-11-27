e176d63e6a5feafce35f15cc9b9c3bc6f0fbdcda
Mirafzal0713 <usmonjonovmirafzal0713@gmail.com>
2023-11-27 18:02:46 +0500

    fix: in progress

--- pages/product-list/ProductList.js
+++ pages/product-list/ProductList.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-debugger */
 const languageButton = document.querySelector('.language-button')
 const languageContent = document.querySelector('.language-content')
 const switchers = document.querySelectorAll(
@@ -6,86 +7,20 @@ const switchers = document.querySelectorAll(
 const view_wraps = document.querySelectorAll('.view-wrap')
 const list_view = document.querySelector('.list-view')
 const grid_view = document.querySelector('.grid-view')
-const card_image = document.getElementsByClassName('card-image')
+const listWrapper = document.getElementById('list-view__wrapper')
+const gridWrapper = document.getElementById('grid-view__wrapper')
+const cardIcons = document.getElementById('grid_icon')
 
-const card_address = document.getElementsByClassName('card-address')
-const card_title = document.getElementsByClassName('card-title')
-const card_date = document.getElementsByClassName('card-date')
-const card_phoneNum = document.getElementsByClassName('card-phoneNum')
-const card_price = document.getElementsByClassName('card-price')
-
-const productList = [
-  {
-    id: 0,
-    name: 'Shirt',
-    slug: 'Shirt',
-    price: 10000000,
-    photo:
-      'https://admin.77.uz/media/cache/34/4a/344ab7a151defd972d85af9c59dad663.jpg',
-    published_at: '2023-11-16T15:24:37.758522+05:00',
-    address: {
-      district: {
-        id: 0,
-        name: 'Sirg`ali tumani',
-        region: {
-          id: 0,
-          name: 'Toshkent shahri',
-        },
-      },
-      name: 'Yangixayot tumani',
-      lat: 43.0,
-      long: 43.0,
-    },
-    seller: {
-      id: 0,
-      full_name: "Qodirov Axrorbek A'zamovich",
-      phone_number: '+998770075521',
-      profile_photo: null,
-    },
-    extra: {
-      is_mine: false,
-      status: 'active',
-      expires_at: '14.02.2024',
-    },
-    is_liked: false,
-    updated_time: '2023-11-16T16:32:00.134939+05:00',
-  },
-  {
-    id: 1,
-    name: 'T-Shirt',
-    slug: 'T-Shirt',
-    price: 2000000,
-    photo:
-      'https://admin.77.uz/media/cache/34/4a/344ab7a151defd972d85af9c59dad663.jpg',
-    published_at: '2023-11-16T15:24:37.758522+05:00',
-    address: {
-      district: {
-        id: 1,
-        name: 'Sirg`ali tumani',
-        region: {
-          id: 1,
-          name: 'Toshkent shahri',
-        },
-      },
-      name: 'Yangixayot tumani',
-      lat: 43.0,
-      long: 43.0,
-    },
-    seller: {
-      id: 1,
-      full_name: "Ergashev Axrorbek A'zamovich",
-      phone_number: '+998770075521',
-      profile_photo: null,
-    },
-    extra: {
-      is_mine: false,
-      status: 'active',
-      expires_at: '14.02.2024',
-    },
-    is_liked: false,
-    updated_time: '2023-11-16T16:32:00.134939+05:00',
-  },
-]
+document.addEventListener('DOMContentLoaded', function () {
+  // Fetch the JSON file
+  fetch('./ProductList.json')
+    .then((response) => response.json())
+    .then((data) => {
+      // Once the data is loaded, call a function to render it
+      productGetList(data.productList)
+    })
+    .catch((error) => console.error('Error fetching data:', error))
+})
 
 languageButton.addEventListener('click', (e) => {
   e.preventDefault()
@@ -114,19 +49,72 @@ switchers.forEach(function (switcher) {
 })
 
 function productGetList(productList) {
-  productList.map((product) => {
-    card_image[0].src = product.photo
-    card_image[1].src = product.photo
-    card_address[0].innerText = product.address.district.name
-    card_address[1].innerText = product.address.district.name
-    card_title[0].innerText = product.name
-    card_title[1].innerText = product.name
-    card_date[0].innerText = dateFormatter(product.published_at)
-    card_date[1].innerText = dateFormatter(product.published_at)
-    card_phoneNum[0].innerText = product.seller.phone_number
-    card_phoneNum[1].innerText =  product.seller.phone_number
-    card_price[0].innerHTML = `${product.price}    <span> UZS </span>`
-    card_price[1].innerText = product.price
+  productList.forEach((product) => {
+    const list_card = document.createElement('div')
+    const grid_card = document.createElement('div')
+    list_card.classList.add('list-view__card')
+    grid_card.classList.add('grid-view__card')
+
+    // ----------------Card list style--------------------
+    list_card.innerHTML = `
+            <div class="list-view__card-symbols">
+              <img class="list-view__card-image card-image" src="${
+                product.photo
+              }" alt="Product photo" />
+            </div>
+            <div class="list-view__card-content">
+              <div class="list-view__top-content">
+                <h3 class="list-view__card-title card-title">
+               ${product.name}
+                </h3>
+                <span class="badge card-address"> ${
+                  product.address.district.name
+                }</span>
+              </div>
+              <div class="list-view__bottom-content">
+                <p class="list-view__card-price card-price">
+               ${product.price}<span> UZS </span>
+                </p>
+                <div>
+                  <p class="list-view__card-date card-date"> ${dateFormatter(
+                    product.published_at,
+                  )}</p>
+                  <a class="list-view__card-number card-phoneNum" href="tel:${
+                    product.seller.phone_number
+                  }">${product.seller.phone_number}</a>
+                </div>
+              </div>
+            </div>`
+    listWrapper.appendChild(list_card)
+
+    // ----------------Card grid style--------------------
+    grid_card.innerHTML = `
+            <div class="grid-view__card-symbols">
+              <div id="grid_icon" class="grid-view__card-icon">
+              <iconify-icon id="icon"  icon="tabler:heart-filled"></iconify-icon>
+              </div>
+              <img class="grid-view__card-image card-image" src="${
+                product.photo
+              }" alt="Product photo" />
+            </div>
+            <div class="grid-view__card-content">
+              <span class="badge card-address">${
+                product.address.district.name
+              }</span>
+              <h3 class="grid-view__card-title card-title">${product.name}</h3>
+              <p class="grid-view__card-date card-date">${dateFormatter(
+                product.published_at,
+              )}</p>
+              <a class="grid-view__card-number card-phoneNum" href="tel:${
+                product.seller.phone_number
+              }">${product.seller.phone_number}</a>
+              <p class="grid-view__card-price card-price"> ${
+                product.price
+              }<span> UZS </span></p>
+            </div>`
+    gridWrapper.appendChild(grid_card)
+
+    // ---------------- Is Like Function -----------------
   })
 }
 
@@ -146,4 +134,17 @@ function dateFormatter(date) {
   return formattedDate
 }
 
-productGetList(productList)
+// Loop through each element and add a click event listener
+console.log(cardIcons)
+for (let i = 0; i < cardIcons.length; i++) {
+  debugger
+  cardIcons[i].addEventListener('click', handleCardIconClick)
+}
+
+// Your function to be called on icon click
+function handleCardIconClick() {
+  debugger
+
+  // Your logic for the click event
+  alert('Icon clicked!')
+}
