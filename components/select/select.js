18ab809185db3ecdfc5f2ca23c67cd495bda6f36
Zohid-Jurakulovich <zohidjurakulovich@gmail.com>
2023-11-30 12:54:35 +0500

    Add tick icon to selected option

--- components/select/select.js
+++ components/select/select.js
@@ -6,10 +6,19 @@ selectContext = optionMenu.querySelector(".select-menu__context");
 selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
 
 options.forEach(option =>{
-  option.addEventListener("click", ()=>{
+  option.addEventListener("click", () => {
+    
+    options.forEach((otherOption) => {
+      if (otherOption !== option) {
+        otherOption.classList.remove('selected-option');
+    }
+    })
+    
+    
     let selectedOption = option.querySelector(".option-text").innerText;
     selectContext.innerText = selectedOption;
-    
+    option.classList.add('selected-option');
+
     optionMenu.classList.remove("active");
   })
 })
