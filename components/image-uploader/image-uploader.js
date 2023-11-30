dc95bf0a39a8b3040cc93685abb33922c6559301
zawkindev <zawkindev@gmail.com>
2023-11-30 11:05:29 -0800

    feat: make gallery items draggabel

--- components/image-uploader/image-uploader.js
+++ components/image-uploader/image-uploader.js
@@ -1,59 +1,18 @@
 const dropBox = document.querySelector("#dropbox");
 const container = document.querySelector("#dropbox__container");
-const img_elements = document.querySelectorAll("img");
-const array1 = [dropBox, container];
 
-function render() {
-  const chooseTexts = document.querySelectorAll(".choose-text");
-  const emptyCards = document.querySelectorAll(".gallery__item-empty");
-  const array = [chooseTexts, emptyCards];
-
-  img_elements.forEach(element => {
-    element.draggable = false
-  })
-
-  array.forEach((array_item) => {
-    array_item.forEach((el) => {
-      el.addEventListener("click", () => {
-        dropBox.style.display = "flex";
-      });
-    });
-  });
-}
-
-array1.forEach((element) => {
-  element.addEventListener("click", () => {
-    dropBox.style.display = "none";
-  });
-  element.addEventListener("drop", (e) => {
-    e.preventDefault();
-    emptyCards[0].src = e.dataTransfer.files[0].name;
-    dropBox.style.display = "none";
-  });
-  element.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
-  element.addEventListener("dragleave", (e) => {
-    e.preventDefault();
-  });
-  element.addEventListener("dragend", (e) => {
-    e.preventDefault();
-  });
-});
 let dragSrcEl;
 
 function dragStart(e) {
   dragSrcEl = this;
   e.dataTransfer.effectAllowed = "move";
   e.dataTransfer.setData("text/html", this.innerHTML);
-  render();
   return false;
 }
 
 function dragOver(e) {
   e.preventDefault();
   e.dataTransfer.dropEffect = "move";
-  render();
   return false;
 }
 
@@ -62,7 +21,6 @@ function dragDrop(e) {
     dragSrcEl.innerHTML = this.innerHTML;
     this.innerHTML = e.dataTransfer.getData("text/html");
   }
-  render();
   return false;
 }
 
@@ -71,7 +29,6 @@ function dragEnd() {
   [].forEach.call(listItems, function(item) {
     item.draggable = "true";
   });
-  render();
   return false;
 }
 
@@ -87,4 +44,3 @@ const listItems = document.querySelectorAll(".gallery__item");
   addEventsDragAndDrop(item);
 });
 
-render();
