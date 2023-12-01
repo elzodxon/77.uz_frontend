813e6a33cd5a691f150b2b606ec5eeab67bcc1da
zawkindev <zawkindev@gmail.com>
2023-11-30 20:23:58 -0800

    feat: make cards draggabel

--- components/image-uploader/image-uploader.js
+++ components/image-uploader/image-uploader.js
@@ -5,8 +5,8 @@ let files = [];
 
 // Create gallery card
 const createGalleryCard = (id, image) => {
-  return `
-          <div class="gallery__item" draggable="true" ondragstart="dragStart(e)" ondragover="dragOver(e)"  ondrop="dragDrop(e)" ondragend="dragEnd()">
+    return `
+          <div class="gallery__item">
           <div class="gallery__item-image" draggable="true">
             <div class="actions" draggable="false">
               <button  class="actions__button" draggable="false">
@@ -23,39 +23,36 @@ const createGalleryCard = (id, image) => {
 };
 
 // Remove image
-const removeImage = (id) => {
-  files.forEach((element, index) => {
-    if (element.id === id) {
-      files.splice(index, 1);
-    }
-  });
-  renderImages();
-};
+// const removeImage = (id) => {
+//   files.forEach((element, index) => {
+//     if (element.id === id) {
+//       files.splice(index, 1);
+//     }
+//   });
+//   renderImages();
+// };
 
 // Remove image
 
 
-
 // Render images
 const renderImages = () => {
-gallery.innerHTML = ''
-  files.forEach((element) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      const card = document.createElement('div');
-      card.innerHTML = createGalleryCard(element.id, reader.result);
-      gallery.appendChild(card);
-    };
-
-    reader.readAsDataURL(element.file);
-  });
-  makeDraggable()
+    gallery.innerHTML = ''
+    files.forEach((element) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const card = document.createElement('div');
+            card.innerHTML = createGalleryCard(element.id, reader.result);
+            gallery.append(card);
+        };
+        reader.readAsDataURL(element.file);
+    });
 };
 
 // Handle dragenter event on dropbox
 dropbox.addEventListener('dragenter', (e) => {
-  e.preventDefault();
-  dropbox.classList.add('active'); // Add active class to dropbox
+    e.preventDefault();
+    dropbox.classList.add('active'); // Add active class to dropbox
 });
 
 // !!! REMOVED TO FIX BUG: dropbbox disappaering, and image is opening in whole tab !!!
@@ -68,39 +65,38 @@ dropbox.addEventListener('dragenter', (e) => {
 
 // Prevent default behavior for dragover to allow dropping
 dropbox.addEventListener('dragover', (e) => {
-window.removeEventListener('dragenter')
-  e.preventDefault();
+    window.removeEventListener('dragenter')
+    e.preventDefault();
 });
 
 // Handle drop event on dropbox
 dropbox.addEventListener('drop', (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const droppedFiles = e.dataTransfer.files;
+    const droppedFiles = e.dataTransfer.files;
 
-  if (droppedFiles.length > 0) {
+    if (droppedFiles.length > 0) {
+        const file = {
+            id: Math.random() * 1000,
+            file: droppedFiles[0],
+        };
+
+        files.push(file);
+
+    }
+
+    dropbox.classList.remove('active');
+});
+
+fileInput.addEventListener('change', (e) => {
     const file = {
-      id: Math.random() * 1000,
-      file: droppedFiles[0],
+        id: Math.random() * 1000,
+        file: e.target.files[0],
     };
 
     files.push(file);
 
     renderImages();
-  }
-
-  dropbox.classList.remove('active');
-});
-
-fileInput.addEventListener('change', (e) => {
-  const file = {
-    id: Math.random() * 1000,
-    file: e.target.files[0],
-  };
-
-  files.push(file);
-
-  renderImages();
 });
 
 // Event delegation for dynamically created delete buttons
@@ -114,55 +110,50 @@ fileInput.addEventListener('change', (e) => {
 // });
 
 
-
 let dragSrcEl;
 
 function dragStart(e) {
-
-  dragSrcEl = this;
-  e.dataTransfer.effectAllowed = "move";
-  e.dataTransfer.setData("text/html", this.innerHTML);
-  return false;
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", this.innerHTML);
+    return false;
 }
 
 function dragOver(e) {
-
-  e.preventDefault();
-  e.dataTransfer.dropEffect = "move";
-  return false;
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+    return false;
 }
 
 function dragDrop(e) {
-  if (dragSrcEl != this) {
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData("text/html");
-  }
+    if (dragSrcEl != this) {
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData("text/html");
+    }
 
-  return false;
+    return false;
 }
 
 function dragEnd() {
-  const listItems = document.querySelectorAll(".gallery__item");
-  [].forEach.call(listItems, function(item) {
-    item.draggable = "true";
-  });
+    const listItems = document.querySelectorAll(".gallery__item");
+    [].forEach.call(listItems, function (item) {
+        item.draggable = "true";
+    });
 
-  return false;
+    return false;
 }
 
 function addEventsDragAndDrop(el) {
-  el.addEventListener("dragstart", dragStart, false);
-  el.addEventListener("dragover", dragOver, false);
-  el.addEventListener("drop", dragDrop, false);
-  el.addEventListener("dragend", dragEnd, false);
+    el.addEventListener("dragstart", dragStart, false);
+    el.addEventListener("dragover", dragOver, false);
+    el.addEventListener("drop", dragDrop, false);
+    el.addEventListener("dragend", dragEnd, false);
 }
 
-function makeDraggable(){
-
-  const listItems = document.getElementsByClassName(".gallery__item");
-  [].forEach.call(listItems, function(item) {
-    addEventsDragAndDrop(item);
-  });
-  console.log(listItems)
-
-}
+window.addEventListener("DOMNodeInserted", () => {
+    const listItems = document.querySelectorAll(".gallery__item");
+    listItems.forEach(el => {
+        addEventsDragAndDrop(el)
+    })
+    console.log(listItems)
+})
