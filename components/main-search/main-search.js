ef70ed823e951f0340dd9926d38f9c71fde30f7b
Iskandar-tulkinov <iscandardev@gmail.com>
2023-12-04 11:32:08 +0500

    add search link to search-suggestion__item in order to go to product-list

--- components/main-search/main-search.js
+++ components/main-search/main-search.js
@@ -1,18 +1,19 @@
 //  MAIN SEARCH
-const blocker = document.querySelector('.blocker');
-  const searchInput = document.getElementById('searchNow');
+const blocker = document.querySelector('.blocker')
+const searchInput = document.getElementById('searchNow')
 
-searchInput.addEventListener('focus', (e) => {e.preventDefault();
-  blocker.classList.toggle('active');
-  searchSuggestion.classList.toggle('active');
-  document.body.classList.toggle('block');
-});
+searchInput.addEventListener('focus', (e) => {
+  e.preventDefault()
+  blocker.classList.toggle('active')
+  searchSuggestion.classList.toggle('active')
+  document.body.classList.toggle('block')
+})
 
 blocker.addEventListener('click', () => {
-  document.body.classList.remove('block');
-  blocker.classList.remove('active');
-  searchSuggestion.classList.remove('active');
-});
+  document.body.classList.remove('block')
+  blocker.classList.remove('active')
+  searchSuggestion.classList.remove('active')
+})
 const jsonData = [
   {
     id: 25,
@@ -319,79 +320,83 @@ const jsonData = [
         ],
       },
     ],
-  }
-
+  },
 ]
 
 /////////------SEARCH SUGGESTION-------///////
 
 const searchSuggestion = document.getElementById('m-search-suggestion')
-  const mSearchSuggestionList = document.querySelector(
-    '.m-search-suggestion__list',
-  )
+const mSearchSuggestionList = document.querySelector(
+  '.m-search-suggestion__list',
+)
 
 // Attach event listener to the search input
-searchInput.addEventListener("input", handleSearchInput);
+searchInput.addEventListener('input', handleSearchInput)
 
 function handleSearchInput() {
-   searchInput.classList.toggle('active')
-  const searchTerm = searchInput.value.trim().toLowerCase();
-  const suggestionList = document.querySelector(".m-search-suggestion__list");
-    // Update "top-txt" value
-    const topTxt = document.querySelector(".top-txt");
-    topTxt.textContent = "Рекомендации";
+  searchInput.classList.toggle('active')
+  const searchTerm = searchInput.value.trim().toLowerCase()
+  const suggestionList = document.querySelector('.m-search-suggestion__list')
+  // Update "top-txt" value
+  const topTxt = document.querySelector('.top-txt')
+  topTxt.textContent = 'Рекомендации'
   // If search input is empty
-  if (searchTerm === "") {
-    suggestionList.innerHTML = "<li class='m-search-suggestion__item' style='background-color: #fff;cursor:default'>No history</li>";
-    topTxt.textContent = "История поиска";
-    return;
+  if (searchTerm === '') {
+    suggestionList.innerHTML =
+      "<li class='m-search-suggestion__item' style='background-color: #fff;cursor:default'>No history</li>"
+    topTxt.textContent = 'История поиска'
+    return
   }
   // Check if there are matches in jsonData
-  const matches = getMatchingValues(searchTerm);
-  displaySearchResults(matches);
+  const matches = getMatchingValues(searchTerm)
+  displaySearchResults(matches)
 }
 
 function getMatchingValues(searchTerm) {
-  const matches = [];
+  const matches = []
   // Check for matches in jsonData
   jsonData.forEach((category) => {
     if (category.name.toLowerCase().includes(searchTerm)) {
-      matches.push(category.name);
+      matches.push(category.name)
     }
     if (category.childs) {
       category.childs.forEach((child) => {
         if (child.name.toLowerCase().includes(searchTerm)) {
-          matches.push(child.name);
+          matches.push(child.name)
         }
         if (child.childs) {
           child.childs.forEach((subChild) => {
             if (subChild.name.toLowerCase().includes(searchTerm)) {
-              matches.push(subChild.name);
+              matches.push(subChild.name)
             }
-          });
+          })
         }
-      });
+      })
     }
-  });
-  return matches;
+  })
+  return matches
 }
 
 function displaySearchResults(matches) {
-  const suggestionList = document.querySelector(".m-search-suggestion__list");
+  const suggestionList = document.querySelector('.m-search-suggestion__list')
 
   if (matches.length > 0) {
     suggestionList.innerHTML = `${matches
-      .map((match) => `<li class="m-search-suggestion__item">
+      .map(
+        (match) => `<li class="m-search-suggestion__item">
       <svg class="search-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M14.1057 14.2L17 17M16.0667 9.53333C16.0667 13.1416 13.1416 16.0667 9.53333 16.0667C5.92507 16.0667 3 13.1416 3 9.53333C3 5.92507 5.92507 3 9.53333 3C13.1416 3 16.0667 5.92507 16.0667 9.53333Z" stroke="#388FF3" stroke-width="1.5" stroke-linecap="round"/>
       </svg>
+       <a class="suggestion-link" href="/pages/product-list/ProductList.html">
       ${highlightMatch(match)}
+      </a>
       <svg class="right-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M9.5999 7.19995L14.3999 12L9.5999 16.8" stroke="#B8BBBD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg></li>
 
-`)
-      .join("")}`;
+`,
+      )
+      .join('')}`
   } else {
     suggestionList.innerHTML = `
     <div class="search-error" style="cursor: default;">
@@ -399,12 +404,15 @@ function displaySearchResults(matches) {
     <h3 class="nothing-found">Ничего не найдено</h3>
     <span class="cant-find">Упс! Мы не смогли найти ни одного <br> подходящего результата по вашему запросу</span>
     </div>
-    `;
+    `
   }
 }
 
 function highlightMatch(value) {
-  const searchTerm = searchInput.value.trim();
-  const regex = new RegExp(searchTerm, "gi");
-  return value.replace(regex, (match) => `<span class="matching-letters" >${match}</span>`);
+  const searchTerm = searchInput.value.trim()
+  const regex = new RegExp(searchTerm, 'gi')
+  return value.replace(
+    regex,
+    (match) => `<span class="matching-letters" >${match}</span>`,
+  )
 }
