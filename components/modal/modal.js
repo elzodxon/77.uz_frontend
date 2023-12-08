08a8dd93963ec2366033707d0863ee23ab87cf20
Asranov Baxtinur <asranov0003@gmail.com>
2023-12-09 01:36:15 +0500

    fix: add all modals

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,35 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const modalButton = document.getElementById('modal')
-  const modal = document.querySelector('.modal')
-  const overlay = document.querySelector('.overlay')
-  const closeButton = document.querySelector('.close-btn')
-  const cancelButton = document.querySelector('.btn-secondary')
-  const deleteButton = document.querySelector('.btn-danger')
+const overlay = document.querySelector('.overlay')
 
-  function openModal() {
-    modal.style.display = 'block'
-    overlay.style.display = 'block'
-    document.addEventListener('keydown', handleKeyDown)
+function openModal(modalId) {
+  const modal = document.getElementById(modalId)
+  modal.style.display = 'block'
+  overlay.style.display = 'block'
+  document.addEventListener('keydown', (event) => handleKeyDown(event, modalId))
+}
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId)
+  modal.style.display = 'none'
+  overlay.style.display = 'none'
+  document.removeEventListener('keydown', (event) =>
+    handleKeyDown(event, modalId),
+  )
+}
+
+function handleKeyDown(event, modalId) {
+  if (event.key === 'Escape' || event.keyCode === 27) {
+    closeModal(modalId)
   }
+}
 
-  function closeModal() {
-    modal.style.display = 'none'
-    overlay.style.display = 'none'
-    document.removeEventListener('keydown', handleKeyDown)
-  }
-
-  function handleKeyDown(event) {
-    if (event.key === 'Escape' || event.keyCode === 27) {
-      closeModal()
-    }
-  }
-
-  modalButton.addEventListener('click', openModal)
-  closeButton.addEventListener('click', closeModal)
-  overlay.addEventListener('click', closeModal)
-  cancelButton.addEventListener('click', closeModal)
-  deleteButton.addEventListener('click', function () {
-    console.log('Delete button clicked')
-    closeModal()
-  })
-})
+overlay.addEventListener('click', closeModal)
