ada99b06acf9b4c2c7ce779f84fb1783457249f8
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2023-12-14 01:49:07 +0500

    fix: modal z-index

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -3,6 +3,7 @@ const overlay = document.querySelector('.overlay')
 function openModal(modalId) {
   const modal = document.getElementById(modalId)
   modal.style.display = 'block'
+  modal.style.zIndex = '1000'
   overlay.style.display = 'block'
   document.addEventListener('keydown', (event) => handleKeyDown(event, modalId))
 }
@@ -10,6 +11,7 @@ function openModal(modalId) {
 function closeModal(modalId) {
   const modal = document.getElementById(modalId)
   modal.style.display = 'none'
+  modal.style.zIndex = '0'
   overlay.style.display = 'none'
   document.removeEventListener('keydown', (event) =>
     handleKeyDown(event, modalId),
