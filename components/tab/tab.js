538f00136033410052163034c24ea48e0847abd7
Xasanov Ibrohim <xasanovibrohim555@gmail.com>
2023-12-18 16:34:42 +0500

    fix: language switcher varible name

--- components/tab/tab.js
+++ components/tab/tab.js
@@ -1,15 +1,15 @@
 const switcherButton = document.querySelectorAll('.language-switcher__input')
 const languageSwitcher = document.querySelectorAll('.language-switcher')
-const overlay = document.querySelector('.language-switcher__active')
+const overlaySwitcher = document.querySelector('.language-switcher__active')
 
 for (let i = 0; i < languageSwitcher.length; i++) {
   languageSwitcher[i].addEventListener('click', function () {
     if (switcherButton[i].value === 'russian') {
-      overlay.classList.add(`russian`)
-      overlay.classList.remove('english')
+      overlaySwitcher.classList.add(`russian`)
+      overlaySwitcher.classList.remove('english')
     } else if (switcherButton[i].value === 'english') {
-      overlay.classList.add(`english`)
-      overlay.classList.remove('russian')
+      overlaySwitcher.classList.add(`english`)
+      overlaySwitcher.classList.remove('russian')
     }
   })
 }
